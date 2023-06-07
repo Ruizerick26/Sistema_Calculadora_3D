b26334fafad8e019069ec2200bcef7cc14a63f8c
Ruizerick26 <ruizerick2525@gmail.com>
2023-06-06 20:57:25 -0500

    Inicialización de atributos y constructor

---
+++ src/Menu_Sistema.java
@@ -0,0 +1,5 @@
+public class Menu_Sistema {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Metodos_Sistema.java
@@ -0,0 +1,15 @@
+public class Metodos_Sistema {
+    //Variables-atributos
+    private float lado; //Usar para calcular perimetros de bases como cubo, trapecio, piramide
+    private float altura; // usar para calcular piramides y cilindro
+    private float radio; // Usar para esfera y cilidro
+    private float area;
+    private float perimtero;
+    private float volumen;
+
+    public Metodos_Sistema(float lado, float altura, float radio) {
+        this.lado = lado;
+        this.altura = altura;
+        this.radio = radio;
+    }
+}
