77afc5c63c9047832a17576352bd9ca85d842705
DANNY\LENOVO <danny9yanacallo@gmail.com>
2023-06-08 20:53:50 -0500

    commit

--- src/Menu_Sistema.java
+++ src/Menu_Sistema.java
@@ -1,5 +1,11 @@
 public class Menu_Sistema {
     public static void main(String[] args) {
+        Metodos_Sistema cubo=new Metodos_Sistema(4, 8, 9);
+        cubo.setLado(5);
+        System.out.println("Area del cubo: "+ cubo.area_cubo(6));
+        System.out.println("Perimetro del cubo: "+ cubo.perimetro_cubo(6));
+        System.out.println("Volumen del cubo: "+ cubo.volumen_cubo(6));
+
 
     }
 }
