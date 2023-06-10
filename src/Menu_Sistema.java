dbcacc83db61a1dcb6eb1d3a8bb797d0163f5203
Ruizerick26 <ruizerick2525@gmail.com>
2023-06-09 21:50:50 -0500

    Primer avance del menu

--- src/Menu_Sistema.java
+++ src/Menu_Sistema.java
@@ -1,11 +1,47 @@
+import java.util.Scanner;
+
 public class Menu_Sistema {
     public static void main(String[] args) {
-        Metodos_Sistema cubo=new Metodos_Sistema(4, 8, 9);
-        cubo.setLado(5);
-        System.out.println("Area del cubo: "+ cubo.area_cubo(6));
-        System.out.println("Perimetro del cubo: "+ cubo.perimetro_cubo(6));
-        System.out.println("Volumen del cubo: "+ cubo.volumen_cubo(6));
+        Scanner tomar = new Scanner(System.in);
+        menu();
+        System.out.println("Ingrese la opcion que desee:");
+        int opcion = tomar.nextInt();
+        while (opcion!=5){
+            switch (opcion) {
+                case 1:
+                    //Siguir este case para el resto
+                    System.out.println("CUBO");
+                    Metodos_Sistema cubo = new Metodos_Sistema(4, 8, 9);
+                    System.out.println("Ingrese el valor de un lado del cubo");
+                    float lado = tomar.nextFloat();
+                    cubo.setLado(lado);
+                    System.out.println("Area del cubo: " + cubo.area_cubo(cubo.getLado()));
+                    System.out.println("Perimetro del cubo: " + cubo.perimetro_cubo(cubo.getLado()));
+                    System.out.println("Volumen del cubo: " + cubo.volumen_cubo(cubo.getLado()));
+                    break;
+                case 2:
+                    System.out.println("PIRAMIDE");
+                    break;
+                case 3:
+                    System.out.println("CILINDRO");
+                    break;
+                case 4:
+                    System.out.println("ESFERA");
+                    break;
 
-
+            }
+            menu();
+            System.out.println("Ingrese la opcion que desee:");
+            opcion = tomar.nextInt();
+        }
+        System.out.println("GRACIAS POR USAR EL SISTEMA");
+    }
+    public static void menu(){
+        System.out.println("-----Sistema Calculadora de figuras 3D-----\n\n");
+        System.out.println("1.- Calcular el area y volumen de un Cubo\n");
+        System.out.println("2.- Calcular el area y volumen de un Piramide\n");
+        System.out.println("3.- Calcular el area y volumen de un Cilindro\n");
+        System.out.println("4.- Calcular el area y volumen de un Esfera\n");
+        System.out.println("5.- Salir\n");
     }
 }
