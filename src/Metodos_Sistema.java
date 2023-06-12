2676c59a8b138f547233610d22089a2ff19fbbac
brittanypallasco2003 <nohemiespinel0@gmail.com>
2023-06-12 18:47:18 -0500

    Métodos de la pirámide y actulización del constructor

--- src/Metodos_Sistema.java
+++ src/Metodos_Sistema.java
@@ -1,19 +1,23 @@
 public class Metodos_Sistema {
 
     //Variables-atributos
-    private float lado; //Usar para calcular perimetros de bases como cubo, trapecio, piramide
-    private float altura; // usar para calcular piramides y cilindro
-    private float radio; // Usar para esfera y cilidro
-    private float area;
-    private float perimtero;
+    private float lado; //Usar para calcular perímetros de bases como cubo, trapecio, piramide
+    private float altura; // usar para calcular pirámides y cilindro
+    private float radio; // Usar para esfera y cilindro
+    private float area_base;//Usar para área y volumen de una pirámide
+    private float perimtero_base;//Usar área de una pirámide
+    private float apotema; //Usar para área de una pirámide
     private float volumen;
     private float pi = 3.14f;
     private double areaSuperficial;
 
-    public Metodos_Sistema(float lado, float altura, float radio, float areaSuperficial) {
+    public Metodos_Sistema(float lado, float altura, float radio, float area_base, float perimtero_base, float apotema, float areaSuperficial) {
         this.lado = lado;
         this.altura = altura;
         this.radio = radio;
+        this.area_base=area_base;
+        this.perimtero_base=perimtero_base;
+        this.apotema=apotema;
         this.areaSuperficial=areaSuperficial;
     }
     //Getters and Setters
@@ -33,6 +37,24 @@ public class Metodos_Sistema {
     public float volumen_cubo(float lado){
         return this.lado*this.lado*this.lado;
     }
+
+    public float getArea_base() {return area_base;}
+    public void setArea_base(float area_base) {this.area_base = area_base;}
+    public float getPerimtero_base() {return perimtero_base;}
+    public void setPerimtero_base(float perimtero_base) {this.perimtero_base = perimtero_base;}
+    public float getApotema() {return apotema;}
+    public void setApotema(float apotema) {this.apotema = apotema;}
+
+    private float calcularAreaP(){return area_base+((perimtero_base*apotema)/2);}
+    public float getArea(){
+        return calcularAreaP();
+    }
+
+    private double calcularVolumenP(){
+        return 0.3333*area_base*altura;
+    }
+
+    public double getVolumenP(){return calcularVolumenP();}
 
     public float getAltura() {
         return altura;
@@ -71,4 +93,7 @@ public class Metodos_Sistema {
     public double getAreaSuperficial() {
         return areaSuperficial;
     }
+
+
+
 }
